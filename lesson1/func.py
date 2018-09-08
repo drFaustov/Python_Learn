d32bec7954e7c20bbfb3a77064b9cf31752be4e2
Alexey <alexey.faustov@gmail.com>
2018-09-08 16:47:10 +0300

    first attempts to learn python

---
+++ lesson1/func.py
@@ -0,0 +1,5 @@
+def get_summ(one, two, delimeter='&'):
+    return (str(one) + str(delimeter) + str(two)).upper()
+
+print(get_summ("Learn", "python"))
+        
---
+++ lesson1/info.py
@@ -0,0 +1,4 @@
+user_info = {'first name': 'hkjhjk', 'last name': 'kjhkjjkh'}
+
+#print(user_info['last name'])
+print(f'Last name is: {user_info["last name"]}')
