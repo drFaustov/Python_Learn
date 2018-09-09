bdd7e5d621c3808719037c7e3c39c6d5180fddc6
Alexey <alexey.faustov@gmail.com>
2018-09-09 22:00:31 +0300

    Age check task, ui_load module minor changes

--- lesson2/ui_load.py
+++ lesson2/ui_load.py
@@ -1,8 +1,4 @@
-import os
-import sys
-
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QWidget
-from PyQt5.QtWidgets import QLineEdit, QPushButton, QVBoxLayout, QHBoxLayout, QAction, qApp
+from PyQt5.QtWidgets import QWidget, QLineEdit, QPushButton, QVBoxLayout
 
 
 class AgeWidget(QWidget):
@@ -27,18 +23,19 @@ class AgeWidget(QWidget):
         self.check_button.clicked.connect(self.check_age)
 
         self.setLayout(v_layout)
-        self.setWindowTitle('Occupation prediction based on occupation check')
+        self.setWindowTitle('Occupation prediction based on age check')
+        self.setFixedWidth(600)
 
         self.show()
 
     def check_age(self):
         age = int(self.line_edit.text())
-        age_message_tuple = ({'age': 7, 'message': "Where are your parents kid?"},
-                       {'age': 16 , 'message': "Why aren't you in school?"},
-                       {'age': 23 , 'message': "Why aren't you in school?"},
-                       {'age': 65 , 'message': "Why aren't you in school?"},
-                       {'age': 100, 'message': "You can rest!"},
-                       {'age': -1, 'message': "Your age is incorrect"})
+        age_message_tuple = ({'age':   7, 'message': "Where are your parents kid?"},
+                             {'age':  16, 'message': "Why aren't you in school?"},
+                             {'age':  23, 'message': "Don't miss lectures you'll need them further?"},
+                             {'age':  65, 'message': "Do you have a sick leave?"},
+                             {'age': 100, 'message': "You can rest!"},
+                             {'age':  -1, 'message': "Your age is incorrect"})
         for i in range(len(age_message_tuple)):
             if age > age_message_tuple[i]['age'] and i < len(age_message_tuple)-1:
                 continue
